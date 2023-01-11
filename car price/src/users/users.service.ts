4f00a1ce169a0536177aa9eb254c206ed39a3b25
sparsh-18 <sparsh.red@gmail.com>
2023-01-11 17:10:18 +0530

    Car price app user signup created

---
+++ car price/src/users/users.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private repo: Repository<User>
+    ) {}
+
+    create(email: string, password: string) {
+        const user = this.repo.create({email, password});
+
+        return this.repo.save(user);
+    }
+}
