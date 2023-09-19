cca0ee16b340127f6f3e9fef9a146b9e0c5c99da
Sparsh Verma <sparsh.verma@viamagus.com>
2023-09-19 14:36:39 +0530

    updated dependencies, added config

--- car price/src/app.module.ts
+++ car price/src/app.module.ts
@@ -6,14 +6,30 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true,
+    // }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'sqlite',
+        // type: config.get<string>('DB_TYPE'), not working
+        database: config.get<string>('DB_NAME'),
+        entities: [User, Report],
+        synchronize: true,
+        logging: true,
+      }),
     }),
     UsersModule,
     ReportsModule,
