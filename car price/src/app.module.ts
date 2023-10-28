47e2531883bee0647f038e723f6b987df24e3c89
Sparsh Verma <sparsh.verma@viamagus.com>
2023-10-28 22:06:43 +0530

    used config for cookie key

--- car price/src/app.module.ts
+++ car price/src/app.module.ts
@@ -48,7 +48,8 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(cookieSession({ keys: ['abcd'] })).forRoutes('*');
+    consumer.apply(cookieSession({ keys: [this.configService.get('COOKIE_KEY')] })).forRoutes('*');
   }
 }
